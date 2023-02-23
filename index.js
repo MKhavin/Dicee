2f54333011d7f96978f2675141ddb9fc745168fe
MichaelK <havinmichael@gmail.com>
2023-02-23 23:38:35 +0200

    Add functionality to check winner and show to user that via changing title.

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-function getRandomValue() {
-    let randomValue = Math.floor(Math.random() * 6 + 1 );
-    let imageName = "dice" + randomValue + ".png";
-
-    return imageName;
-}
-
 function setDiceValueFor(player) {
-    let diceValue = getRandomValue();
+    let diceValue = Math.floor(Math.random() * 6) + 1;
+    let imageName = "dice" + diceValue + ".png";
+
     let elements = document.getElementsByClassName(player);
 
     if (elements.length > 0) {
-        let element = elements[0].setAttribute("src", "images/" + diceValue);
+        let element = elements[0].setAttribute("src", "images/" + imageName);
+
+        return diceValue;
     }
+
+    return 6;
 }
 
-setDiceValueFor("player1-img");
-setDiceValueFor("player2-img");
+function setWinner(player1Value, player2Value) {
+    var title = "";
+
+    if (player1Value === player2Value) {
+        title = "Draw";
+    } else if (player1Value > player2Value) {
+        title = "🚩 Player 1 Wins!";
+    } else {
+        title = "Player 2 Wins! 🚩";
+    }
+
+    let element = document.querySelector("h1");
+    element.textContent = title;
+}
+
+let player1Value = setDiceValueFor("player1-img");
+let player2Value = setDiceValueFor("player2-img");
+
+setWinner(player1Value, player2Value);
